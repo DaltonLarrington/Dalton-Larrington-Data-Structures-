d0098f3fd38ab85c3065bda3274b041de93c397b
Dalton Larrington <dalton2@graceland.edu>
2018-03-07 12:41:51 -0600

    website update

---
+++ Dalton Larrington JavaScript Website/JavaScript Practice 1/info.js
@@ -0,0 +1,7 @@
+var name = "Dalton Larrington";
+var date = Date();
+
+document.write("<span id ='footer'>Dalton's Website</span><br />");
+document.write("<br />&copy; 2018 Dalton Larrington" + " <br/>Modified on 3-7-2018 <br />");
+document.write("Retrieved on " + date);
+
